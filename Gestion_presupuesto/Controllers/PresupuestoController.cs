6d6f27db907a143b9f7972b4a1d82bcdbd4a3231
moises796 <35872947+moises796@users.noreply.github.com>
2025-10-28 10:01:24 -0600

    Movimiento presupuesto

--- Gestion_presupuesto/Controllers/PresupuestoController.cs
+++ Gestion_presupuesto/Controllers/PresupuestoController.cs
@@ -340,6 +340,7 @@ namespace Gestion_presupuesto.Controllers
                         modelItem.monto = item.monto;
                         modelItem.id_fuente_financiamiento = item.id_fuente_financiamiento;
                         modelItem.id_unidad_organizativa = item.id_unidad_organizativa;
+                        modelItem.motivo_movimiento = item.motivo_movimiento;
                         db.SaveChanges();
                     }
                 }
